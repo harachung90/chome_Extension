262fee172ab9036c49f5f6ddd7dd08ae36649463
Hara Chung <harachung90@gmail.com>
2023-01-26 19:45:11 +1100

    Second commit. Added a function to save the input in the localStorage. Added a manifest.json

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ const inputEl = document.getElementById('input-el')
 let inputBtn = document.getElementById('input-btn')
 let ulEl = document.getElementById('ul-el')
 
+// localStorage.setItem("myLead", "www.example.com")
+// console.log(localStorage.getItem("myLead"))
+localStorage.clear()
 inputBtn.addEventListener("click", function () {
         myLeads.push(inputEl.value)
-        for (var i = 0; i <myLeads.length; i++) {
-            ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-        }
+        inputEl.value = ""
+        renderLeads()
     }
 )
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        //listItems += "<li><a target='_blank' href='" + myLeads[i] + "' >" + myLeads[i] + "</a></li>"
+        listItems += `<li>
+                        <a target='_blank' href='${myLeads[i]}'>
+                            ${myLeads[i]}
+                        </a>
+                      </li>`
+        //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+        //const li = document.createElement("li")
+        //li.textContent = myLeads[i]
+        //ulEl.append(li)
+    }
+    ulEl.innerHTML = listItems
+}
