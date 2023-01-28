54def734fc4d825b79f6a17f0d893f888ce051ab
Hara Chung <harachung90@gmail.com>
2023-01-28 15:03:46 +1100

    Fourth commit. Updated the extension - now the current tab can be saved using the SAVE TAB button

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById('input-el')
 let inputBtn = document.getElementById('input-btn')
+let saveBtn = document.getElementById('save-btn')
+let deleteBtn = document.getElementById('delete-btn')
 let ulEl = document.getElementById('ul-el')
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLead"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLead"))
 
 if (leadsFromLocalStorage) {
     myLead = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-inputBtn.addEventListener("click", function () {
-        myLeads.push(inputEl.value)
-        inputEl.value = ""
-        localStorage.setItem("myLead", JSON.stringify(myLeads))
-        renderLeads()
-        console.log(localStorage.getItem("myLead"))
-    }
-)
+saveBtn.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 
-function renderLeads() {
+
+})
+
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         //listItems += "<li><a target='_blank' href='" + myLeads[i] + "' >" + myLeads[i] + "</a></li>"
         listItems += `<li>
-                        <a target='_blank' href='${myLeads[i]}'>
-                            ${myLeads[i]}
+                        <a target='_blank' href='${leads[i]}'>
+                            ${leads[i]}
                         </a>
                       </li>`
         //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
@@ -35,3 +38,21 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems
 }
+
+inputBtn.addEventListener("click", function () {
+        myLeads.push(inputEl.value)
+        inputEl.value = ""
+        localStorage.setItem("myLead", JSON.stringify(myLeads))
+        render(myLeads)
+        //console.log(localStorage.getItem("myLead"))
+    }
+)
+
+deleteBtn.addEventListener('dblclick', function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+
+})
+
+
